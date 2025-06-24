737330b33fcee885574ec1fc06582f84178fcedc
NickGhignatti <nick.ghignatti@gmail.com>
2025-06-24 15:19:38 +0200

    ♻️ Convert old test into scalatest

--- src/test/scala/model/entities/games/TestGame.scala
+++ src/test/scala/model/entities/games/TestGame.scala
@@ -1,47 +1,44 @@
 package model.entities.games
 
-import org.junit.Assert.*
-import org.junit.Test
+import org.scalatest.funsuite.AnyFunSuite
 import utils.Result.{Failure, Success}
-import utils.{Result, Vector2D}
+import utils.Vector2D
 
-class TestGame {
+class TestGame extends AnyFunSuite {
 
-  @Test
-  def testLockWhenAllowed(): Unit =
+  test("lock should succeed when under capacity") {
     val game = Game("TestGame", Vector2D(0.0, 0.0), GameState(0, 1))
     game.lock() match {
-      case Success(newGame) => assertEquals(1, newGame.gameState.currentPlayers)
-      case _                => fail("Expected success when locking game")
-    }
-
-  @Test
-  def testLockWhenFull(): Unit = {
-    val game = Game("TestGame", Vector2D(0.0, 0.0), GameState(0, 1))
-    val oldGameResult: Result[Game, Game] = game.lock()
-    assertTrue(oldGameResult.isSuccess)
-    val oldGame: Game = oldGameResult.unpack()
-    oldGame.lock() match {
-      case Failure(newGame) => assertEquals(oldGame, newGame)
-      case _                => fail("Expected failure when locking full game")
+      case Success(newGame) => assert(newGame.gameState.currentPlayers === 1)
+      case _                => fail("Expected Success when locking game")
     }
   }
 
-  @Test
-  def testUnlockWhenPossible(): Unit = {
+  test("lock should fail when at capacity") {
+    val game = Game("TestGame", Vector2D(0.0, 0.0), GameState(0, 1))
+    val first = game.lock()
+    assert(first.isSuccess, "First lock should succeed")
+
+    val lockedGame = first.unpack()
+    lockedGame.lock() match {
+      case Failure(newGame) => assert(newGame === lockedGame)
+      case _                => fail("Expected Failure when locking full game")
+    }
+  }
+
+  test("unlock should succeed when players present") {
     val game = Game("TestGame", Vector2D(0.0, 0.0), GameState(1, 1))
     game.unlock() match {
-      case Success(newGame) => assertEquals(0, newGame.gameState.currentPlayers)
-      case _                => fail("Expected success when unlocking game")
+      case Success(newGame) => assert(newGame.gameState.currentPlayers === 0)
+      case _                => fail("Expected Success when unlocking game")
     }
   }
 
-  @Test
-  def testUnlockWhenNotPossible(): Unit = {
+  test("unlock should fail when no players") {
     val game = Game("TestGame", Vector2D(0.0, 0.0), GameState(0, 1))
     game.unlock() match {
-      case Failure(newGame) => assertEquals(game, newGame)
-      case _ => fail("Expected failure when unlocking empty game")
+      case Failure(newGame) => assert(newGame === game)
+      case _ => fail("Expected Failure when unlocking empty game")
     }
   }
 }
