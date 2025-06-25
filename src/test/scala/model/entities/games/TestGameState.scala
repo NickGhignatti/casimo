df4e53c547f0680a779a45e5637ee16b3d126093
NickGhignatti <nick.ghignatti@gmail.com>
2025-06-25 09:35:58 +0200

    ✅ Add test for GameState

---
+++ src/test/scala/model/entities/games/TestGameState.scala
@@ -0,0 +1,39 @@
+package model.entities.games
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.TryValues
+import scala.util.{Success, Failure}
+
+class TestGameState extends AnyFunSuite with TryValues {
+
+  test("addPlayer when empty should succeed and increment currentPlayers") {
+    val gameState = GameState(0, 5)
+    val result = gameState.addPlayer()
+
+    result.getOrElse(gameState).currentPlayers === 1
+  }
+
+  test("addPlayer when full should fail and leave currentPlayers unchanged") {
+    val gameState = GameState(5, 5)
+    val result = gameState.addPlayer()
+
+    result.isFailure
+    result.getOrElse(gameState).currentPlayers === 5
+  }
+
+  test("removePlayer when full should succeed and decrement currentPlayers") {
+    val gameState = GameState(5, 5)
+    val result = gameState.removePlayer()
+
+    result.isSuccess
+    result.getOrElse(gameState).currentPlayers === 4
+  }
+
+  test("removePlayer when empty should fail and leave currentPlayers at zero") {
+    val gameState = GameState(0, 5)
+    val result = gameState.removePlayer()
+
+    result.isFailure
+    result.getOrElse(gameState).currentPlayers === 0
+  }
+}
