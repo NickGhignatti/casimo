72b92ae338028bfcb2199a3ce0390be4cacfe882
Marco Galeri <marco.galeri@studio.unibo.it>
2025-07-03 23:55:48 +0200

    ✨ Add persistenceManager and baseManager structure

--- backend/src/main/scala/model/GlobalConfig.scala
+++ backend/src/main/scala/model/GlobalConfig.scala
@@ -1,3 +1,10 @@
 package model
 
-case class GlobalConfig ()
+case class GlobalConfig(
+    customMovementDef: Map[
+      String,
+      Any /*MovementStrategy*/
+    ], // Or just movement params
+    customStrategyDef: Map[String, Any /*CustomerGameStrategy*/ ]
+    // Other static params that are configured at the start that we could need
+)
--- backend/src/main/scala/model/managers/PersistenceManager.scala
+++ backend/src/main/scala/model/managers/PersistenceManager.scala
@@ -1,3 +1,13 @@
 package model.managers
+import model.GlobalConfig
+import model.entities.CustState.Idle
+import model.entities.{BoredomFrustration, CustomerState}
 
-class PersistenceManager
+//TODO: add complex behaviour, maybe based on riskProfile
+class PersistenceManager[A <: BoredomFrustration[A] & CustomerState[A]]
+    extends BaseManager[Seq[A]]:
+  def update(customers: Seq[A])(using config: GlobalConfig): Seq[A] =
+    customers.map { c =>
+      if c.boredom > 80 || c.frustration > 60 then c.changeState(Idle)
+      else c
+    }
