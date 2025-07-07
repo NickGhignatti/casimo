b8372242b88a05097b7159378d7f195eadba5a95
Marco Galeri <marco.galeri@studio.unibo.it>
2025-07-07 23:56:29 +0200

    ✅ Refactor and add test to persistenceManager

--- backend/src/main/scala/model/managers/PersistenceManager.scala
+++ backend/src/main/scala/model/managers/PersistenceManager.scala
@@ -3,6 +3,7 @@ import model.GlobalConfig
 import model.entities.customers.Bankroll
 import model.entities.customers.BoredomFrustration
 import model.entities.customers.CustState.Idle
+import model.entities.customers.CustState.Playing
 import model.entities.customers.CustomerState
 
 //TODO: implement reasonable rule like if you have less bankroll than the minimum bet of the game
@@ -11,7 +12,10 @@ class PersistenceManager[
 ] extends BaseManager[Seq[A]]:
   def update(customers: Seq[A])(using config: GlobalConfig): Seq[A] =
     customers.map { c =>
-      if c.boredom > 80 || c.frustration > 60 || c.bankroll <= 0.0 then
-        c.changeState(Idle)
-      else c
+      c.customerState match
+        case Playing(game) =>
+          if c.boredom > 80 || c.frustration > 60 || c.bankroll < 0.1 /* game.gameState.minimumBet*/
+          then c.changeState(Idle)
+          else c
+        case Idle => c
     }
--- backend/src/test/scala/model/managers/TestPersistenceManager.scala
+++ backend/src/test/scala/model/managers/TestPersistenceManager.scala
@@ -29,43 +29,60 @@ case class MockCustomer(
   protected def updatedBankroll(newBankroll: Double): MockCustomer =
     this.copy(bankroll = newBankroll)
 
+val mockGame = Game("test",Vector2D.zero,GameState(1,8))
 class TestPersistenceManager extends AnyFunSuite:
   test("Customer should leave the table when too bored"):
     given config: GlobalConfig = GlobalConfig()
     val testPersistenceManager = PersistenceManager[MockCustomer]()
     val mockCustomer = MockCustomer(
-      customerState = Playing(Game("test",Vector2D.zero,GameState(1,8))),
+      customerState = Playing(mockGame),
       boredom = 81,
       frustration = 50,
       bankroll = 40.0
     )
     val shouldBeIdle = testPersistenceManager.update(Seq(mockCustomer)).head
-    assert(shouldBeIdle.customerState == Idle)
+    assert(shouldBeIdle.customerState === Idle)
 
   test("Customer should leave the table when too frustrated"):
     given config: GlobalConfig = GlobalConfig()
 
     val testPersistenceManager = PersistenceManager[MockCustomer]()
     val mockCustomer = MockCustomer(
-      customerState = Playing(Game("test",Vector2D.zero,GameState(1,8))),
+      customerState = Playing(mockGame),
       boredom = 30,
       frustration = 61,
       bankroll = 40.0
     )
     val shouldBeIdle = testPersistenceManager.update(Seq(mockCustomer)).head
-    assert(shouldBeIdle.customerState == Idle)
+    assert(shouldBeIdle.customerState === Idle)
 
-  test("Customer should not change state if not bored or frustrated"):
+  test("Customer shouldn't stop playing if not bored or frustrated"):
 
     given config: GlobalConfig = GlobalConfig()
 
     val testPersistenceManager = PersistenceManager[MockCustomer]()
     val mockCustomer = MockCustomer(
-      customerState = Playing(Game("test",Vector2D.zero,GameState(1,8))),
+      customerState = Playing(mockGame),
       boredom = 50,
       frustration = 50,
       bankroll = 40.0
     )
     val shouldStillPlay = testPersistenceManager.update(Seq(mockCustomer)).head
-    assert(shouldStillPlay.customerState == Playing)
+    assert(shouldStillPlay.customerState === Playing(mockGame))
 
+  test("Customer should leave the table if is bankroll is too low"):
+
+    given config: GlobalConfig = GlobalConfig()
+
+    val testPersistenceManager = PersistenceManager[MockCustomer]()
+    val mockCustomer = MockCustomer(
+      customerState = Playing(mockGame),
+      boredom = 50,
+      frustration = 50,
+      bankroll = 40.0
+    )
+    val shouldStillPlay = testPersistenceManager.update(Seq(mockCustomer)).head
+    val lowBankroll = shouldStillPlay.updateBankroll(-40.0)
+    val shouldLeave = testPersistenceManager.update(Seq(lowBankroll)).head
+    assert(shouldStillPlay.customerState === Playing(mockGame))
+    assert(shouldLeave.customerState === Idle)
