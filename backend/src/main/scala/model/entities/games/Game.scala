b491030260912639f797810ce647062097b8b0e0
NickGhignatti <nick.ghignatti@gmail.com>
2025-07-07 16:18:27 +0200

    ✨ Add game type

--- backend/src/main/scala/model/entities/games/Game.scala
+++ backend/src/main/scala/model/entities/games/Game.scala
@@ -1,27 +1,39 @@
 package model.entities.games
 
 import model.entities.Entity
+import model.entities.games.GameType.{Blackjack, Roulette, SlotMachine}
 import utils.Result
 import utils.Result.Success
 import utils.Vector2D
 
+enum GameType:
+  case SlotMachine
+  case Roulette
+  case Blackjack
+
 class Game(
     val id: String,
     val position: Vector2D,
-    val gameState: GameState
+    val gameState: GameState,
+    val strategy: GameStrategy
 ) extends Entity:
+
+  def getGameType: GameType = strategy match
+    case _: RouletteStrategyInstance  => Roulette
+    case _: SlotStrategyInstance      => SlotMachine
+    case _: BlackJackStrategyInstance => Blackjack
 
   def lock(): Result[Game, Game] = gameState.addPlayer() match
     case Success(newGameState) =>
       Result.Success(
-        Game(id, position, newGameState)
+        Game(id, position, newGameState, strategy)
       )
     case _ => Result.Failure(this)
 
   def unlock(): Result[Game, Game] = gameState.removePlayer() match
     case Success(newGameState) =>
       Result.Success(
-        Game(id, position, newGameState)
+        Game(id, position, newGameState, strategy)
       )
     case _ => Result.Failure(this)
 
--- backend/src/test/scala/model/entities/games/TestGame.scala
+++ backend/src/test/scala/model/entities/games/TestGame.scala
@@ -1,18 +1,21 @@
 package model.entities.games
 
+import model.entities.games.dsl.use
 import org.scalatest.funsuite.AnyFunSuite
 import utils.Result.{Failure, Success}
 import utils.Vector2D
 
 class TestGame extends AnyFunSuite:
+  val strategy: SlotStrategyInstance = use(SlotStrategy) bet 5.0 when (true)
+
   test("lock should succeed when under capacity"):
-    val game = Game("TestGame", Vector2D(0.0, 0.0), GameState(0, 1))
+    val game = Game("TestGame", Vector2D(0.0, 0.0), GameState(0, 1), strategy)
     game.lock() match
       case Success(newGame) => assert(newGame.gameState.currentPlayers === 1)
       case _                => fail("Expected Success when locking game")
 
   test("lock should fail when at capacity"):
-    val game = Game("TestGame", Vector2D(0.0, 0.0), GameState(0, 1))
+    val game = Game("TestGame", Vector2D(0.0, 0.0), GameState(0, 1), strategy)
     val first = game.lock()
     assert(first.isSuccess, "First lock should succeed")
 
@@ -22,13 +25,13 @@ class TestGame extends AnyFunSuite:
       case _                => fail("Expected Failure when locking full game")
 
   test("unlock should succeed when players present"):
-    val game = Game("TestGame", Vector2D(0.0, 0.0), GameState(1, 1))
+    val game = Game("TestGame", Vector2D(0.0, 0.0), GameState(1, 1), strategy)
     game.unlock() match
       case Success(newGame) => assert(newGame.gameState.currentPlayers === 0)
       case _                => fail("Expected Success when unlocking game")
 
   test("unlock should fail when no players"):
-    val game = Game("TestGame", Vector2D(0.0, 0.0), GameState(0, 1))
+    val game = Game("TestGame", Vector2D(0.0, 0.0), GameState(0, 1), strategy)
     game.unlock() match
       case Failure(newGame) => assert(newGame === game)
       case _ => fail("Expected Failure when unlocking empty game")
