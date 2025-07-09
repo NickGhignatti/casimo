1294b7cb0b714fb1566dfed29bdd43cf1e52b123
NickGhignatti <nick.ghignatti@gmail.com>
2025-07-09 15:29:49 +0200

    ✨ Add game resolver
    add game resolver, a simple method for customers and games to interact

---
+++ backend/src/main/scala/model/entities/games/GameResolver.scala
@@ -0,0 +1,24 @@
+package model.entities.games
+
+import model.entities.customers.{CustState, Customer}
+import utils.Result
+import utils.Result.Failure
+
+object GameResolver:
+  def update(customers: List[Customer], games: List[Game]): List[Game] =
+    // TODO: waiting for APIs
+    val playingCustomers = customers.filter(customer =>
+      customer.customerState match
+        case CustState.Playing(game) => true
+        case CustState.Idle          => false
+    )
+    playingCustomers.map { customer =>
+      customer.customerState match
+        case CustState.Playing(game) =>
+          game.play(SlotBet(10.0)) match // TODO
+            case Result.Success(value) =>
+              value match
+                case Result.Success(winValue)  => game.updateHistory(winValue)
+                case Result.Failure(lostValue) => game.updateHistory(-lostValue)
+            case Result.Failure(error) => game.updateHistory(0.0)
+    }
