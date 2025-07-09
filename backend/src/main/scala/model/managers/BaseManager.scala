a5475156ccf266bb2d904a122a84a53b6403d99b
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2025-07-09 12:07:42 +0200

    🎨 Add | operator for concatenating managers and to pass its input to a manager

--- backend/src/main/scala/model/managers/BaseManager.scala
+++ backend/src/main/scala/model/managers/BaseManager.scala
@@ -4,3 +4,11 @@ import model.GlobalConfig
 
 trait BaseManager[A]:
   def update(slice: A)(using config: GlobalConfig): A
+
+  def |(second: BaseManager[A]): BaseManager[A] =
+    new BaseManager[A]:
+      override def update(slice: A)(using config: GlobalConfig): A =
+        second.update(this.update(slice))
+
+extension [A](slice: A)
+  def |(manager: BaseManager[A])(using GlobalConfig): A = manager.update(slice)
