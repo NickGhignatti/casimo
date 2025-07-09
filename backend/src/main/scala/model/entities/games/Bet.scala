7b3313293b1385555ceb6c161a1b1e80a7209c14
NickGhignatti <nick.ghignatti@gmail.com>
2025-07-09 11:50:28 +0200

    ✨ Update bet type

--- backend/src/main/scala/model/entities/games/Bet.scala
+++ backend/src/main/scala/model/entities/games/Bet.scala
@@ -1,9 +1,13 @@
 package model.entities.games
 
+import utils.Result
+
+type BetResult = Result[Double, Double]
+
 trait Bet:
   val amount: Double
 
-case class FixedBet(amount: Double) extends Bet:
+case class SlotBet(amount: Double) extends Bet:
   require(amount > 0, "Bet amount must be positive")
 
 case class RouletteBet(amount: Double, targets: List[Int]) extends Bet:
@@ -13,3 +17,7 @@ case class RouletteBet(amount: Double, targets: List[Int]) extends Bet:
     targets.forall(t => t >= 0 && t <= 36),
     "Targets must be between 0 and 36"
   )
+
+case class BlackJackBet(amount: Double, minimumValue: Int) extends Bet:
+  require(amount > 0, "Bet amount must be positive")
+  require(minimumValue > 0, "Cards value must be positive")
