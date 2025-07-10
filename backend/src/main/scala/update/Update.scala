35042aa361c80730da76fd71d15c02e1a0136301
NickGhignatti <nick.ghignatti@gmail.com>
2025-07-10 12:27:56 +0200

    ♻️ Remove useless println

--- backend/src/main/scala/update/Update.scala
+++ backend/src/main/scala/update/Update.scala
@@ -16,7 +16,6 @@ object Update:
   def update(state: SimulationState, event: Event): SimulationState =
     event match
       case SimulationTick =>
-        println("Simulation tick event received, updating state...")
         state.spawner match
           case None => update(state, UpdateCustomersPosition)
           case Some(value) if value.customerQuantity == state.customers.size =>
@@ -24,23 +23,18 @@ object Update:
           case Some(value) =>
             update(value.spawn(state), UpdateCustomersPosition)
       case UpdateCustomersPosition =>
-        println("Updating customers' positions...")
         given GlobalConfig = GlobalConfig()
         val boidManager = DefaultMovementManager()
         val newCustPos = boidManager.update(state.customers)
         update(state.copy(customers = newCustPos), UpdateGames)
       case UpdateGames =>
-        println("Updating games...")
         update(state, UpdateSimulationBankrolls)
       case UpdateSimulationBankrolls =>
-        println("Updating simulation bankrolls...")
         update(state, UpdateCustomersState)
       case UpdateCustomersState =>
-        println("Updating customers' state...")
         state
 
       case AddCustomers(n) =>
-        println("Adding customers to the state...")
         state.copy(spawner =
           Some(Spawner(Random.nextString(12), Vector2D.zero, n, 10))
         )
